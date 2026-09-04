39adbdce376ada583a0a9f2bb47f947f9a96bfaa
abel tiruneh <abeltiruneh2468@gmail.com>
2026-09-04 10:35:48 +0300

    fixed the table name misiing on order status histeory

--- app/Models/OrderStatusHistory.php
+++ app/Models/OrderStatusHistory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderStatusHistory extends Model
 {
+    protected $table = 'order_status_history';
     protected $fillable = [
         'order_id',
         'status',
