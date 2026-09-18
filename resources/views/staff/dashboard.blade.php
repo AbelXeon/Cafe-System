409e2ea5acc1bbe49b10dccb296e205d0afaa3d4
abel tiruneh <abeltiruneh2468@gmail.com>
2026-09-18 12:19:08 +0300

    removed unessery comment form the code 16

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -10,7 +10,6 @@
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js"></script>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
