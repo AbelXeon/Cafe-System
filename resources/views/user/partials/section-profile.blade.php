426977f913b645ef051d9c396e3239d2234c7a40
abel tiruneh <abeltiruneh2468@gmail.com>
2026-09-22 15:46:12 +0300

    removed uneeser code form the code comments 33

--- resources/views/user/partials/section-profile.blade.php
+++ resources/views/user/partials/section-profile.blade.php
@@ -145,7 +145,6 @@
             </form>
         </div>
 
-        <!-- Security & Password Card -->
         <div class="bg-[#14131a] border border-[#2a2731] rounded-2xl p-5 sm:p-7 shadow-xl">
             <div class="flex items-center justify-between pb-5 border-b border-[#1e1c25]">
                 <div class="flex items-center gap-3">
