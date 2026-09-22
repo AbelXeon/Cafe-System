5ab8ff9adba0594b2d1456b97d42b7218bbe6c9f
abel tiruneh <abeltiruneh2468@gmail.com>
2026-09-22 17:30:13 +0300

    removed uneseery comments in the code 3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -240,7 +240,6 @@
                     </div>
                 </div>
 
-                <!-- Beverage -->
                 <div class="group relative rounded-2xl overflow-hidden bg-[#0f0e13] border border-[#1e1c25] h-80 flex flex-col justify-end p-5">
                     <img
                         src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?auto=format&fit=crop&w=600&q=60&fm=webp"
