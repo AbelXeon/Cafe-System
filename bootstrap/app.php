a1205b476359b7b9fdcb11deb84adbd0b60353cb
abel tiruneh <abeltiruneh2468@gmail.com>
2026-09-23 12:31:48 +0300

    deleted unnessery comments in the code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Exclude Telegram routes from CSRF token checks
         $middleware->validateCsrfTokens(except: [
             'telegram/*',
              'internal/*',
